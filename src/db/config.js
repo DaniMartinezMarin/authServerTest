2e957ec332efe5cf21785965580a81998bbb4dd4
Daniel Martinez <dmartmar@mercadona.es>
2022-07-29 14:01:26 +0200

    feat: Anyadido volumen de bbdd para no perder datos

--- src/db/config.js
+++ src/db/config.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dbConnection = async() => {
 
     await mongoose.connect( process.env.BBDD_CNN )
-            .then(db => console.log('BBDD eeee a', db.connection.host))
+            .then(db => console.log('BBDD conectada a', db.connection.host))
             .catch(err => {
                 console.error(err);
                 throw new Error('Error al inicializar la BBDD');
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { dbConnection } = require('./db/config');
 require('dotenv').config(); //El paquete dotenv permite leer las variables de entorno del fichero .env
 
-//Crear el servidor/aplicación de express
+// Crear el servidor/aplicación de express
 const app = express();
 
 // Base de datos
@@ -21,6 +21,7 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
+// Este puerto estará en el contenedor, para conectarse a él hay que utilizar el de la app definida en el compose
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
