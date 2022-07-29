330f886520c35926fbfd7942d76ca840fdf15464
Daniel Martinez <dmartmar@mercadona.es>
2022-07-29 13:32:23 +0200

    feat: Anyadido docker al proyecto para generar imagen con la aplicacion y la BBDD

---
+++ src/db/config.js
@@ -0,0 +1,16 @@
+const { log } = require("console");
+const mongoose = require("mongoose");
+
+const dbConnection = async() => {
+
+    await mongoose.connect( process.env.BBDD_CNN )
+            .then(db => console.log('BBDD eeee a', db.connection.host))
+            .catch(err => {
+                console.error(err);
+                throw new Error('Error al inicializar la BBDD');
+            });
+}
+
+module.exports = {
+    dbConnection
+}
